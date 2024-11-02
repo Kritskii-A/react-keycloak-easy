f7c31ea6476bbcbc9899fec36d725ec3420c17a3
AKritskiy <AKritskiy@nexign.com>
2024-11-02 14:09:15 +0200

    delete

--- lib/web/context.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const reactKeycloakWebContext: import("react").Context<import("..").IAuthContextProps<import("keycloak-js").default>>;
-export declare const ReactKeycloakWebContextConsumer: import("react").Consumer<import("..").IAuthContextProps<import("keycloak-js").default>>;
