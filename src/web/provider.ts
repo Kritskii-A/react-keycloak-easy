4e1ce447945680fce530d1c173151eba958336e7
Aleksandr Kritskii <akritskii@nexign.com>
2025-02-28 17:41:45 +0200

    feat: correct type for react 19

--- src/web/provider.ts
+++ src/web/provider.ts
@@ -1,7 +1,10 @@
+import { FC } from "react";
 import { createAuthProvider } from "../core";
-
 import { reactKeycloakWebContext } from "./context";
+import type { AuthProviderProps } from "../core/provider";
+import type Keycloak from "keycloak-js";
 
-export const ReactKeycloakProvider = createAuthProvider(
-  reactKeycloakWebContext
-);
+export const ReactKeycloakProvider: FC<AuthProviderProps<Keycloak>> =
+  createAuthProvider(reactKeycloakWebContext) as unknown as FC<
+    AuthProviderProps<Keycloak>
+  >;
